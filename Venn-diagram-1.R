1beb2c7825b3bf3b64bf606d70f8a32ca9d3c3a0
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-01 07:43:47 +0530

    Update Venn-diagram-1.R

--- Venn-diagram-1.R
+++ Venn-diagram-1.R
@@ -4,16 +4,20 @@ library(limma)
 # take a dataset from ucla
 hsb2 <- read.csv("https://stats.idre.ucla.edu/wp-content/uploads/2016/02/hsb2-3.csv") 
 attach(hsb2)
+# take high writing scores >=60)
+hw <- (write >= 60)
+# take high maths scores >=60)
+hm <- (math >= 60)
+# take high read with scores >=60)
+hr <- (read >= 60)
+# take high science with scores >=60)
+hs <- (science >= 60)
+# take high social science with scores >=60)
+ho <- (socst >= 60)
 # use limma package
-hw <- (write >= 50)
+c5 <- cbind(hw, hm, hr, hs, ho)
 # use limma package
-hm <- (math >= 50)
-# use limma package
-hr <- (read >= 50)
-# use limma package
-c3 <- cbind(hw, hm, hr)
-# use limma package
-a <- vennCounts(c3)
+a <- vennCounts(c5)
 # print a 
 a
 # use  vennDiagram package
